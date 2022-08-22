457ea8230cd622ded445f6adaff419e74d17f981
tardesm <mstreltsova@mail.ru>
2022-08-22 11:13:18 +0300

    fixed bug and added output

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 dir="$1"
-if [ -d $dir ]: then
+if [ -d $dir ]; then
        echo -e "Checking disk usage in $dir\n"
-else:
+else
        echo -e "$dir is not a directory!\n"
        exit 101
-fi:
+fi
+
+echo "The heaviest subfolder of $dir is:"
+top1=$(du /home/pystud35 -d 1 2>/dev/null | sort -rn | head -n 2 | tail -n 1 | cut -f 2)
+disk=$(du /home/pystud35 -d 1 2>/dev/null | sort -rn | head -n 2 | tail -n 1 | cut -f 1)
+echo -e "$top1 with disk usage $disk"
